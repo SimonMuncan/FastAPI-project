a3b4f91cfac3ae22381b505d7e21433490e530c5
SimonMuncan <simonmuncan@gmail.com>
2025-02-17 12:45:08 +0100

    feat: created POST project, GET all projects, GET specific project, PUT update project, DELETE project

--- src/main.py
+++ src/main.py
@@ -1,8 +1,55 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+class Project(BaseModel):
+    project_id: int
+    name: str
+    description: str | None = None
+
 
 app = FastAPI()
+users = [{"name": "Simon", "id": "1"}]
+projects = []
 
 
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
+
+
+@app.post("/projects/")
+async def create_project(project: Project) -> object:
+    projects.append(project)
+    return project
+
+
+@app.get("/projects/")
+async def get_all_projects() -> list:
+    return projects
+
+
+@app.get("/project/{project_id}/info")
+async def get_project_details(project_id: int) -> object:
+    project = next((p for p in projects if p.project_id == project_id), None)
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return project
+
+
+@app.put("/project/{project_id}/info")
+async def update_project_details(project_id: int, description: str) -> object:
+    project = next((p for p in projects if p.project_id == project_id), None)
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project not found")
+    project.description = description
+    return project
+
+
+@app.delete("/project/{project_id}/")
+async def delete_project(project_id: int) -> dict[str, str]:
+    project = next((p for p in projects if p.project_id == project_id), None)
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project not found")
+    projects.remove(project)
+    return {"message": "Deleted project"}
